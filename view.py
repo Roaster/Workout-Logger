78c2af8d755457f6ce4444a5279771b89c86bc18
Roaster <bchenze@csu.fullerton.edu>
2022-12-08 12:19:48 -0800

    Working on the front end

--- view.py
+++ view.py
@@ -1,13 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 
 views = Blueprint(__name__, "views")
 
-
-#Can pass variables like username with the render template function
-@views.route("/")
+workouts = []
+@views.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("index.html", username = "Brandon")
+    
+    if request.method == "POST":
+        workout = str(request.form['workout'])
+        weight = str(request.form['weight'])
+        reps = str(request.form['reps'])
+        message = "You did " + workout + " at " + weight + " pounds for " + reps + " reps"
+        workouts.append(message)
+        print(workouts)
+        return render_template("index.html", message=message, workouts=workouts)
+    else:
+        return render_template("index.html", message = 'None')
+
+
 
 @views.route("/info")
 def info():
-    return "Welcome to the info page. This application was built to practice."
+    return "Welcome to the info page. This application was built to practice web development."
